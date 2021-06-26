f3c2d4882468e85471b4119246f01c72eb80bb1b
resyfer <palsaurav.2020@gmail.com>
2021-06-26 16:39:28 +0530

    Finished apply section

---
+++ client/src/components/Table.jsx
@@ -0,0 +1,43 @@
+import "./Table.css";
+
+const Table = () => {
+
+  return (
+    <table id="apply-table">
+
+      <thead>
+        
+        <tr>
+          <td>Round</td>
+          <td>Fees</td>
+          <td>Open From</td>
+        </tr>
+        
+      </thead>
+
+      <tbody>
+
+        <tr>
+          <td>Early Bird</td>
+          <td>₹250</td>
+          <td>9th-14th Feb 2021</td>
+        </tr>
+
+        <tr>
+          <td>Round 1</td>
+          <td>₹300</td>
+          <td>14th-20th Feb 2021</td>
+        </tr>
+
+        <tr>
+          <td>Round 2</td>
+          <td>₹350</td>
+          <td>20th-25th Feb 2021</td>
+        </tr>
+
+      </tbody>
+    </table>
+  )
+}
+
+export default Table;
