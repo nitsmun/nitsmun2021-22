200b9de214e8e1ad4ae19ce2203adc0b43c4f834
resyfer <palsaurav.2020@gmail.com>
2021-07-13 14:44:25 +0530

    Initial removal prep

--- routes/application.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-//example backend route
-router.get('/hello', (req, res) => {
-    res.json({"message":"Hello world!"})
-})
-
-module.exports = router;
