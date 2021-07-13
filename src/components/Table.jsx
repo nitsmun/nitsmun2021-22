a82dbc40081e980ebd1bb8875c15db427e1c6e28
Aditya Kotari <aditya.kotari@gmail.com>
2021-07-13 16:36:28 +0530

    small visual changes to home, about us

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -20,19 +20,19 @@ const Table = () => {
         <tr className="lower-border">
           <td>Early Bird</td>
           <td><span className="rupee">₹</span> 250</td>
-          <td>9th-14th Feb 2021</td>
+          <td>9-14th Feb 2021</td>
         </tr>
 
         <tr className="lower-border">
           <td>Round 1</td>
           <td><span className="rupee">₹</span> 300</td>
-          <td>14th-20th Feb 2021</td>
+          <td>14-20th Feb 2021</td>
         </tr>
 
         <tr>
           <td>Round 2</td>
           <td><span className="rupee">₹</span> 350</td>
-          <td>20th-25th Feb 2021</td>
+          <td>20-25th Feb 2021</td>
         </tr>
 
       </tbody>
