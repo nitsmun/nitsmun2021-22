3350a703cc98882300b80f18c6ff28702997b61a
Biju Borah <biju20_ug@ee.nits.ac.in>
2021-07-26 15:12:15 +0530

    Changed texts for NITSMUN U18 in home page

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -13,19 +13,17 @@ function Update() {
                         <h2 style={{ fontVariant: "small-caps" }}>NITSMUN - Under 18 Edition</h2>
                         <div className="line"></div>
                     </div>
-                    <div className="heading" style={{textAlign:"center",overflowX:"hidden",padding: "0 30px"}}>
-                        <h2 style={{borderBottom: "2px solid black",fontVariant: "small-caps"}}>NITSMUN - Under 18 Edition</h2>
+                    <div className="heading" style={{ textAlign: "center", overflowX: "hidden", padding: "0 30px" }}>
+                        <h2 style={{ borderBottom: "2px solid black", fontVariant: "small-caps" }}>NITSMUN - Under 18 Edition</h2>
                     </div>
                     <div className="row no-gutters">
                         <div className="col-md-1"></div>
                         <div className="col-md-10" style={{ textAlign: "center", overflowX: "hidden" }}>
                             <div style={{ padding: "30px" }}>
-                                <p style={{fontSize: "1.1rem" }}>
-                                    After hosting multiple successful conferences in both online and offline modes, NITSMUN will be conducting its first ever Model United Nations conference exclusively for school students. We at NITSMUN believe that children should be exposed to the various ways the society, nation and the world function. Only with proper grooming, experience and exposure can they make a change in the society.
-                                    <br /><br />
-                                    The NITSMUN - Under 18 Edition will be held from 16-17th October 2021, focusing and helping shape the young minds and prepare them for all their future endeavours.
-                                    <br /><br />
-                                    For more details, head over to our <a href="https://www.instagram.com/nitsmun/">@nitsmun</a> instagram page!
+                                <p style={{ fontSize: "1.1rem" }}>
+                                    With great pride and honour, we announce the first edition of NITS Junior MUN exclusively for school students. We believe that children and young teens should be exposed to the various intricacies of society and nations and should understand how the world functions from the grassroot level along with the challenges faced. It is only with proper grooming, experience and exposure that enables them to make changes in the society. Through this conference, we are targeting students from grade VI to grade XII from schools all across the country.<br/><br/>
+
+                                    A day-long free-of-cost workshop will be held a few days before the conference in order to acclimatise those without prior experience. Keeping in mind the various examination schedules of the Central and State Boards, we have planned to organise our conference from October 16, 2021 to October 17, 2021. The cost of participation for the MUN conference has been set at ₹150 for individuals, with a special discounted offer of ₹125 per individual for group registrations. (discount only applicable to groups with 5 or more participants)
                                 </p>
                             </div>
                         </div>
