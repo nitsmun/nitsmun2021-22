c79cbbc0085e8f6ae32b504df98a0a2a6648555d
Biju Borah <biju20_ug@ee.nits.ac.in>
2021-07-29 13:06:07 +0530

    Added carousel to GvMun and MockMun

--- src/components/screens/GlobalVoicesMun.jsx
+++ src/components/screens/GlobalVoicesMun.jsx
@@ -77,6 +77,60 @@ const GlobalVoicesMun = () => {
             </React.Fragment>
           )}
         </div> */}
+
+        <div className="container my-4">
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
+                    aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4"
+                    aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5"
+                    aria-label="Slide 6"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6"
+                    aria-label="Slide 7"></button>
+            </div>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img loading="lazy" src="/img/2020GV1.jpg" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/2020GV2.jpg" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/2020GV3.jpg" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/2020GV4.jpg" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/2020GV5.jpg" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/2020GV6.jpg" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/2020GV7.jpg" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+        </div>
       </div>
     </Suspense>
   );
--- src/components/screens/MockMun.jsx
+++ src/components/screens/MockMun.jsx
@@ -84,6 +84,64 @@ const MockMun = () => {
             display : `${btn.display}`
           }}
         >{btn.name}</Link>
+        <div className="container my-4">
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
+                    aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4"
+                    aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5"
+                    aria-label="Slide 6"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6"
+                    aria-label="Slide 7"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="7"
+                    aria-label="Slide 8"></button>
+            </div>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img loading="lazy" src="/img/2021MM0.jpg" width="500px" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/2021MM1.jpg" width="500px" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/aoty2020_1.jpg" width="500px" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/aoty2020_2.jpg" width="500px" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/bg1_year.jpg" width="500px" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/bg2_year.jpg" width="500px" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/bg3_year.jpg" width="500px" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+                <div className="carousel-item">
+                    <img loading="lazy" src="/img/bg2_2020.JPG" width="500px" className="d-block img-fluid" alt="NITS MUN"/>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+        </div>
       </div>
     </Suspense>
   );
