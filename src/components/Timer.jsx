c2fa923290e997121993722cecfd1a6cb07288c0
resyfer <resyfer.dev@gmail.com>
2021-08-13 09:53:24 +0530

    Added delegates links and the id seek link to apply

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState, Suspense } from 'react'; //? React for the 
 // import { Link } from 'react-router-dom';
 
 // *CSS*
-import "./Timer.css";
+import './Timer.css';
 import Loading from './Loading';
 
 //! let btnAllow = false;
@@ -12,105 +12,115 @@ let btnAllow = true;
 //! Date Format : Year, Month(0-11), Date, Hrs, Mins, Secs, Millisecs
 let timeTo = new Date(2020, 9, 16, 0, 0, 0, 0);
 
-let timeLimit = Math.floor( timeTo / 1000);
+let timeLimit = Math.floor(timeTo / 1000);
 
 //? Days is left as 100 for 2 digit days
 const timeIntervals = [60, 60, 24, 100],
-      timeIntervalName = ["Days", "Hrs", "Mins", "Secs"]; //? In reverse order from timeIntervals
+	timeIntervalName = ['Days', 'Hrs', 'Mins', 'Secs']; //? In reverse order from timeIntervals
 
-// *Countdown Timer Function*      
+// *Countdown Timer Function*
 function countdown() {
-  
-  /*
-  * timeNow is calculated every time countdown() is run, and difference is found.
-  * Time left for each interval is found using forEach and timeIntervals
-  */
-  
-  let timerDisplay = [0, 0, 0, 0]; //! Format: Days, Hrs, Mins, Secs
-  
-  const timeNow = Math.floor(Date.now() / 1000);
-  let timeDiff = timeLimit - timeNow;
+	/*
+	 * timeNow is calculated every time countdown() is run, and difference is found.
+	 * Time left for each interval is found using forEach and timeIntervals
+	 */
 
-  if(timeDiff < 0) {
-    timerDisplay = ["Event Coming Soon!"];
-    //! btnAllow = false;
-  } else {
-    btnAllow = true;
-    timeIntervals.forEach((interval, index)=> {
-      timerDisplay[index] = `${timeDiff % interval}`.padStart(2, "0");
-      timeDiff = Math.floor(timeDiff/ interval);
-    });
-  }
+	let timerDisplay = [0, 0, 0, 0]; //! Format: Days, Hrs, Mins, Secs
 
-  return timerDisplay;
+	const timeNow = Math.floor(Date.now() / 1000);
+	let timeDiff = timeLimit - timeNow;
+
+	if (timeDiff < 0) {
+		timerDisplay = ['Event Coming Soon!'];
+		//! btnAllow = false;
+	} else {
+		btnAllow = true;
+		timeIntervals.forEach((interval, index) => {
+			timerDisplay[index] = `${timeDiff % interval}`.padStart(2, '0');
+			timeDiff = Math.floor(timeDiff / interval);
+		});
+	}
+
+	return timerDisplay;
 }
 
 // *Timer Component*
 const Timer = () => {
+	// *Use State variables*
+	let [display, setDisplay] = useState([0, 0, 0, 0]);
 
-  // *Use State variables*
-  let [display, setDisplay] = useState([0, 0, 0, 0]);
+	useEffect(() => {
+		const timerInt = setInterval(() => {
+			let displayList = countdown();
 
-  useEffect(()=> {
-    const timerInt = setInterval(()=> {
-      let displayList = countdown();
+			setDisplay(displayList.reverse());
+			// ?Reverse since in countdown(), secs is first, and so on
 
-      setDisplay(displayList.reverse());
-      // ?Reverse since in countdown(), secs is first, and so on
+			if (displayList.length === 1) clearInterval(timerInt); // *Stop interval when EXPIRED*
+		}, 1000);
 
-      if(displayList.length === 1) clearInterval(timerInt); // *Stop interval when EXPIRED*
-    }, 1000);
+		return () => clearInterval(timerInt);
+	}, []);
 
-    return () => clearInterval(timerInt);
-  }, []);
-
-  return (
-    <Suspense fallback={Loading}>
-        {/* <div className="timer-heading"
+	return (
+		<Suspense fallback={Loading}>
+			{/* <div className="timer-heading"
           style={{display: "flex",justifyContent: "center",alignItems: "center", width: "100%",backgroundColor: "var(--theme1-100)",height: "70px"}}>
             <h2 style={{fontVariant:"small-caps",color: "white",fontFamily:"var(--font2)",fontWeight:"bold"}}>NITS Junior MUN starts on <span className="bold">{timeTo.toLocaleString().substring(0, 10) }</span></h2>
         </div> */}
-        <div id="timer">
-          {/* {display.length === 4 && <div className="timer-limit">NITS Junior MUN starts on <span className="bold">{timeTo.toLocaleString().substring(0, 10) }</span> !</div>} */}
-          {/* If display.length = 4 => Display Timer
+			<div id='timer'>
+				{/* {display.length === 4 && <div className="timer-limit">NITS Junior MUN starts on <span className="bold">{timeTo.toLocaleString().substring(0, 10) }</span> !</div>} */}
+				{/* If display.length = 4 => Display Timer
               If display.length = 1 => Display Coming Soon */}
-          {display.length === 4 && display.map((interval, index) => {
-            return(
-              <React.Fragment key={index}>
+				{display.length === 4 &&
+					display.map((interval, index) => {
+						return (
+							<React.Fragment key={index}>
+								<div className='timer-int'>
+									<div className='timer-int-value'>
+										<tt>{interval}</tt>
+									</div>{' '}
+									{/* Time left in interval */}
+									<div className='timer-int-label'>
+										{timeIntervalName[index]}
+									</div>{' '}
+									{/* Interval name */}
+								</div>
+								{index < display.length - 1 &&
+									((window.innerWidth < 1000 && index !== 1) ||
+										window.innerWidth >= 1000) && (
+										<div className='timer-int-colon'>:</div>
+									)}
+							</React.Fragment>
+						);
+					})}
 
-                <div className="timer-int">
-                  <div className="timer-int-value"><tt>{interval}</tt></div> {/* Time left in interval */}
-                  <div className="timer-int-label">{timeIntervalName[index]}</div> {/* Interval name */}
-                </div>
-                {(index < display.length -1 && ((window.innerWidth < 1000 && index!==1) || window.innerWidth >= 1000)) && <div className="timer-int-colon">:</div>}
+				{display.length === 1 && (
+					<div className='coming-soon' style={{ textAlign: 'center' }}>
+						Zonal Ambassador and Delegates Applications Out Now!
+					</div>
+				)}
+			</div>
 
-              </React.Fragment>
-              )
-          })}
+			{/* <div className="apply-now">Application for delegates and the free workshop open now!</div> */}
 
-          {display.length === 1 &&
-            <div className="coming-soon" style={{textAlign: "center"}}>
-              Zonal Ambassador Applications Out Now!
-            </div>
-          }
-
-        </div>
-
-        {/* <div className="apply-now">Application for delegates and the free workshop open now!</div> */}
-        
-        {/* {btnAllow && <Link
+			{/* {btnAllow && <Link
           to="/events/mun-u18#apply"
           className="btn apply-btn"
         >Apply Now</Link>} */}
 
-        {btnAllow && <a
-          href="https://forms.gle/dNALB1mLhyhVwMtHA"
-          className="btn apply-btn"
-          target="_blank"
-          rel="noreferrer"
-        >Apply Now</a>}
-    </Suspense>
-  )
-}
+			{btnAllow && (
+				<a
+					href='/events/mun-u18#apply'
+					className='btn apply-btn'
+					// target='_blank'
+					// rel='noreferrer'
+				>
+					Apply Now
+				</a>
+			)}
+		</Suspense>
+	);
+};
 
 export default Timer;
