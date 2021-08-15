a56f087b76ee29f61217e3862108ff54e9685b57
resyfer <resyfer.dev@gmail.com>
2021-08-15 10:51:43 +0530

    Brought committees up

--- src/components/screens/MunU18.jsx
+++ src/components/screens/MunU18.jsx
@@ -15,10 +15,22 @@ let info = {
 	bgpic: '/img/munu18.jpg',
 
 	eventDetails: [
-		`With great pride and honour, we announce the first edition of NITS Junior MUN exclusively for school students. We believe that children and young teens should be exposed to the various intricacies of society and nations and should understand how the world functions from the grassroot level along with the challenges faced. It is only with proper grooming, experience and exposure that enables them to make changes in the society. Through this conference, we are targeting students from grade VI to grade XII from schools all across the country.`,
-		`A day-long free-of-cost workshop will be held a few days before the conference in order to acclimatise those without prior experience. Keeping in mind the various examination schedules of the Central and State Boards, we have planned to organise our conference from October 16, 2021 to October 17, 2021.`,
-		// `A day-long free-of-cost workshop will be held a few days before the conference in order to acclimatise those without prior experience. Keeping in mind the various examination schedules of the Central and State Boards, we have planned to organise our conference from October 16, 2021 to October 17, 2021. The cost of participation for the MUN conference has been set at ₹150 for individuals, with a special discounted offer of ₹125 per individual for group registrations (discount only applicable to groups with 5 or more participants).`,
-		``,
+		<>
+			With great pride and honour, we announce the first edition of NITS Junior
+			MUN exclusively for school students. We believe that children and young
+			teens should be exposed to the various intricacies of society and nations
+			and should understand how the world functions from the grassroot level
+			along with the challenges faced. It is only with proper grooming,
+			experience and exposure that enables them to make changes in the society.
+			Through this conference, we are targeting students from grade VI to grade
+			XII from schools all across the country.
+			<br />
+			<br />A day-long free-of-cost workshop will be held a few days before the
+			conference in order to acclimatise those without prior experience. Keeping
+			in mind the various examination schedules of the Central and State Boards,
+			we have planned to organise our conference from October 16, 2021 to
+			October 17, 2021.
+		</>,
 		<>
 			<h5 style={{ color: 'var(--theme4-100)', fontWeight: 'bold' }}>
 				WANT TO BE THE FACE OF NITS MODEL UNITED NATIONS?
@@ -33,8 +45,9 @@ let info = {
 			zones and publicize NITS Junior MUN by encouraging them to take part.
 			Still confused about your roles? Don't worry, we'll help you with a step
 			by step plan to make it crystal clear. DON'T MISS OUT THE PERKS-
-		</>,
-		<>
+			<br />
+			<br />
+			<br />
 			i) Officially issued Certificates by NITSMUN for all those who complete
 			every task successfully
 			<br />
@@ -111,17 +124,22 @@ const MunU18 = () => {
 				</div>
 
 				<div className='event-details'>
-					{details.map((para, index) => {
-						return (
-							<React.Fragment key={`para${index}`}>
-								{para}
-								<br />
-								<br />
-
-								{/* {index === 1 && <Table/>} */}
-							</React.Fragment>
-						);
-					})}
+					<>
+						{details[0]}
+						<br />
+						<br />
+					</>
+				</div>
+				<div>
+					<Agenda />
+				</div>
+				<br />
+				<div className='event-details'>
+					<>
+						{details[1]}
+						<br />
+						<br />
+					</>
 				</div>
 
 				<div className='buttons' ref={applyBtns}>
@@ -145,9 +163,6 @@ const MunU18 = () => {
           style={{display: "flex",justifyContent: "center",alignItems: "center", width: "100%",backgroundColor: "var(--theme1-100)",height: "70px", margin: "5vh 0"}}>
             <h5 style={{fontVariant:"small-caps",color: "white",fontFamily:"var(--font2)"}}>Applications out soon!</h5>
         </div> */}
-			</div>
-			<div>
-				<Agenda />
 			</div>
 		</Suspense>
 	);
