25cd7b3fb856299e6d2c434bf3c412f6e7113780
resyfer <resyfer.dev@gmail.com>
2021-08-19 16:31:32 +0530

    added table

--- src/components/screens/Dais.jsx
+++ src/components/screens/Dais.jsx
@@ -5,6 +5,7 @@ import Loading from '../Loading';
 
 // *CSS*
 import '../Events.css';
+import '../Table.css';
 import './Dais.css';
 import Workshops from '../Workshops';
 
@@ -17,7 +18,42 @@ const GlobalVoicesMun = props => {
 	return (
 		<Suspense fallback={<Loading />}>
 			{/* <div className='event'>The Dais</div> */}
-			<div style={{marginTop:"var(--navbar-height-pc)"}}><Workshops/></div>
+			<div style={{ marginTop: 'var(--navbar-height-pc)' }}>
+				<Workshops />
+			</div>
+
+			<table id='apply-table'>
+				<thead>
+					<tr>
+						<td>Timings</td>
+						<td>Topics</td>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr className='lower-border'>
+						<td>18 September, 2021 (5PM-7PM)</td>
+						<td>Empathy and Critical Thinking</td>
+					</tr>
+
+					<tr className='lower-border'>
+						<td>19 September, 2021 (5PM-7PM)</td>
+						<td>
+							Importance of SDGs (implementing them at an individual level)
+						</td>
+					</tr>
+
+					<tr className='lower-border'>
+						<td>25 September, 2021 (4PM-7PM)</td>
+						<td>Familiarization with MUN (general ROPs and jargons)</td>
+					</tr>
+
+					<tr className='lower-border'>
+						<td>26 September, 2021 (4PM-7PM)</td>
+						<td>A Mock MUN Session</td>
+					</tr>
+				</tbody>
+			</table>
 		</Suspense>
 	);
 };
