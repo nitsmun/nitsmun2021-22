4f7cd9396d4bed963a1fae4000f53f72c100a6eb
resyfer <palsaurav.2020@gmail.com>
2021-09-28 21:00:38 +0530

    + reports

---
+++ src/components/Reports.jsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+
+import "./Reports.css";
+
+const tabData = {
+  names: ["Day 1", "Day 2"],
+  content: [
+    <>
+      The three day long workshop organised under the module of NITS Junior MUN
+      in collaboration with The Dias started on the 18th of September,2021 and
+      was conducted on Cisco Webex online platform. The first day of the
+      workshop commenced at 5pm with the director of National Institute of
+      Technology, Silchar, Dr. Shivaji Bndopadhyay illustrating the objective
+      behind its conduction. He went on to emphasise on how MUN conferences can
+      be a great tool for the holistic development of school students, who are
+      the primary targets of the NITS Junior MUN. Further, Dr. Wasim Arif,
+      Associate Dean of student Welfare, NIT Silchar, who also happens to be the
+      Faculty Advisor of NITSMUN focused on the objective behind the body’s new
+      venture to conduct a national level conference for the school students
+      across the country.
+      <br />
+      <br />
+      After the brief introduction ceremony of the guest speakers, Tripti
+      Choudhary Vaid, Jasmine Susan Jose, Sakshi Gupta and Yuvraj Rallapalli,
+      they took over the proceedings of the workshop on behalf of the Dias. The
+      topic for the session was “
+      <span className="bold">Empathy and Critical Thinking</span>” and each of
+      the speakers took their turn to emphasize on the psychological impact and
+      importance of empathy and critical thinking in today’s world.The session
+      was kept quite interesting by presentation of slides. Some of the topics
+      covered in the session were how critical thinking and overanalyzing can be
+      differentiated, the role of empathy in critical thinking among many
+      others.
+      <br />
+      <br />
+      The session was kept quite interactive by allowing the audience to drop in
+      questions and doubts in the chat box throughout the entire session. The
+      one and a half hour long Day1 of the workshop ended with a short QnA
+      session followed by a vote of thanks to The DIAS by members of the NITSMUN
+      society.
+    </>,
+    <>
+      The second day of the three-day-long workshop organized under the module
+      of NITS JUNIOR MUN by NITSMUN in collaboration with The Dais commenced on
+      19th September 2021 at 5:00 PM and was conducted on Cisco Webex online
+      platform.
+      <br />
+      <br />
+      After the brief introduction of the facilitators of this workshop - Muskan
+      Ajitsaria, Avhirup Kumar Ghosh and Vaishnavi Suresh, the proceeding of the
+      workshop was taken over by them on behalf of The Dias.
+      <br />
+      <br />
+      The topic of the session was “
+      <span className="bold">Workshop On SDGs and Youth</span>”. The key
+      objectives of the workshop were -
+      <br />
+      <br />
+      <ol>
+        <li>To build understanding with participants on what SDGs are</li>
+        <li>Reflect on how they related to SDGs as youth and individuals.</li>
+        <li>
+          Develop ideas of how youth can work towards the achievement of SDGs,
+          which was achieved with an in-depth discussion among the guest
+          speakers.
+        </li>
+      </ol>
+      <br />
+      Each speaker took their turn to explain the topics for the session. Ms
+      Vaishnavi Suresh made the students aware of sustainable development, the
+      history of SDGs, the difference between sustainable development snd
+      sustainability and the “Global indicator framework”. Then, Mr Avhirup
+      covered the topic about India and SDGs and about the SDG stakeholders.
+      This was followed by Ms Muskan covering the topics of Critical thinking
+      and the different steps youths can take to contribute towards various
+      SDGs.
+      <br />
+      <br />
+      The session was kept quite interesting with the presentation of slides and
+      interactive as the students were allowed to drop their queries and their
+      responses in the chatbox all throughout the session. Day 2 of the workshop
+      ended with a short QnA session followed by a vote of thanks to The DIAS by
+      members of the NITSMUN society.
+    </>,
+  ],
+};
+
+const Reports = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  return (
+    <div className="reports">
+      <div className="heading">Reports</div>
+      <div className="tab-container">
+        <div className="tab-names">
+          {tabData.names.map((name, index) => (
+            <div
+              className={"name" + (index === tabIndex ? " active-name" : "")}
+              onClick={() => setTabIndex(index)}
+            >
+              {name}
+            </div>
+          ))}
+        </div>
+        <div className="tab-content-container">
+          {tabData.content.map((report, index) => (
+            <div
+              className={
+                "tab-content" + (index === tabIndex ? " active-content" : "")
+              }
+            >
+              {report}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reports;
--- src/components/screens/Dais.jsx
+++ src/components/screens/Dais.jsx
@@ -1,70 +1,74 @@
 // *Hooks*
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect } from "react";
 
-import Loading from '../Loading';
+import Loading from "../Loading";
 
 // *CSS*
-import '../Events.css';
-import '../Table.css';
-import './Dais.css';
-import Workshops from '../Workshops';
-import WorkshopsGallery from '../WorkshopsGallery';
+import "../Events.css";
+import "../Table.css";
+import "./Dais.css";
+import Workshops from "../Workshops";
+import Reports from "../Reports";
+import WorkshopsGallery from "../WorkshopsGallery";
 
-const GlobalVoicesMun = props => {
-	useEffect(() => {
-		// *Setting Up Page Title*
-		document.title = props.title;
-	}, [props.title]);
+const GlobalVoicesMun = (props) => {
+  useEffect(() => {
+    // *Setting Up Page Title*
+    document.title = props.title;
+  }, [props.title]);
 
-	return (
-		<Suspense fallback={<Loading />}>
-			{/* <div className='event'>The Dais</div> */}
-			<div style={{ marginTop: 'var(--navbar-height-pc)' }}>
-				<Workshops />
-			</div>
+  return (
+    <Suspense fallback={<Loading />}>
+      {/* <div className='event'>The Dais</div> */}
+      <div style={{ marginTop: "var(--navbar-height-pc)" }}>
+        <Workshops />
+      </div>
 
-			<table id='apply-table' style={{marginTop:'20px'}}>
-				<thead>
-					<tr>
-						<td>Date</td>
-						<td>Time</td>
-						<td>Topics</td>
-					</tr>
-				</thead>
+      <table id="apply-table" style={{ marginTop: "20px" }}>
+        <thead>
+          <tr>
+            <td>Date</td>
+            <td>Time</td>
+            <td>Topics</td>
+          </tr>
+        </thead>
 
-				<tbody>
-					<tr className='lower-border'>
-						<td>18 September, 2021</td>
-						<td>5PM-7PM</td>
-						<td>Empathy and Critical Thinking</td>
-					</tr>
+        <tbody>
+          <tr className="lower-border">
+            <td>18 September, 2021</td>
+            <td>5PM-7PM</td>
+            <td>Empathy and Critical Thinking</td>
+          </tr>
 
-					<tr className='lower-border'>
-						<td>19 September, 2021</td>
-						<td>5PM-7PM</td>
-						<td>
-							Importance of SDGs (implementing them at an individual level)
-						</td>
-					</tr>
+          <tr className="lower-border">
+            <td>19 September, 2021</td>
+            <td>5PM-7PM</td>
+            <td>
+              Importance of SDGs (implementing them at an individual level)
+            </td>
+          </tr>
 
-					<tr className='lower-border'>
-						<td>25 September, 2021</td>
-						<td>4PM-7PM</td>
-						<td>Familiarization with MUN (general ROPs and jargons)</td>
-					</tr>
+          <tr className="lower-border">
+            <td>25 September, 2021</td>
+            <td>4PM-7PM</td>
+            <td>Familiarization with MUN (general ROPs and jargons)</td>
+          </tr>
 
-					<tr className='lower-border'>
-						<td>26 September, 2021</td>
-						<td>4PM-7PM</td>
-						<td>A Mock MUN Session</td>
-					</tr>
-				</tbody>
-			</table>
-			<div>
-				<WorkshopsGallery/>
-			</div>
-		</Suspense>
-	);
+          <tr className="lower-border">
+            <td>26 September, 2021</td>
+            <td>4PM-7PM</td>
+            <td>A Mock MUN Session</td>
+          </tr>
+        </tbody>
+      </table>
+      <div>
+        <WorkshopsGallery />
+      </div>
+      <div>
+        <Reports />
+      </div>
+    </Suspense>
+  );
 };
 
 export default GlobalVoicesMun;
