9729169bcc5c1827f56c52faedd5f7099ff53574
resyfer <resyfer.dev@gmail.com>
2022-01-20 14:34:30 +0530

    changed color, comment nitsmun workshop 2022

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -37,7 +37,7 @@ const Home = (props) => {
         {/* <div className="home-sec" id="apply" style={{paddingTop:"0"}}> */}
         {/* <Timer /> */}
 
-        <div className="home-apply" style={{ paddingTop: "0" }}>
+        {/* <div className="home-apply" style={{ paddingTop: "0" }}>
           <div
             className="container-fluid p-0"
             style={{
@@ -88,7 +88,7 @@ const Home = (props) => {
           >
             Apply Now!
           </a>
-        </div>
+        </div> */}
 
         {/* </div> */}
         {/* update section */}
